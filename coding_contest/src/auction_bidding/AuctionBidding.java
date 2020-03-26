dae3515f5a3256ff37e6e1913dd98869914156f6
albertgreinoecker <albert.greinoecker@gmail.com>
2020-03-26 09:40:42 +0100

    solved level 3

--- coding_contest/src/auction_bidding/AuctionBidding.java
+++ coding_contest/src/auction_bidding/AuctionBidding.java
@@ -39,18 +39,110 @@ public class AuctionBidding {
 		System.out.printf("%s,%d\n", highestBidder, actPrice);
 	}
 
+	static void level2(String in) {
+
+		String[] p = in.split(",");
+		
+		int highestBid = Integer.parseInt(p[0]); //start
+		int actPrice = highestBid;
+		String highestBidder = p[1];
+		for (int i = 1; i < p.length; i = i + 2) {
+			
+			String bidder = p[i];
+			int bid = Integer.parseInt(p[i+1]);
+			if (bidder.equals(highestBidder) )
+			{
+				highestBid = bid;
+			}
+			else if (bid > highestBid)
+			{
+				actPrice = highestBid + 1;
+				highestBid = bid;
+				highestBidder = bidder;
+				
+			} else if (bid == highestBid)
+			{
+				actPrice = highestBid;
+			} else
+			{
+				actPrice = bid + 1;
+			}
+		}
+		System.out.printf("%s,%d\n", highestBidder, actPrice);
+	}
+	
+	static void level3(String in) {
+
+		
+		String[] p = in.split(",");
+		
+		int highestBid = Integer.parseInt(p[0]); //start
+		int actPrice = highestBid;
+		String highestBidder = p[1];
+		String result = String.format("-,%d,%s,%d,", highestBid, highestBidder,highestBid);
+		for (int i = 1; i < p.length; i = i + 2) {
+			
+			String bidder = p[i];
+			int bid = Integer.parseInt(p[i+1]);
+			if (bidder.equals(highestBidder) )
+			{
+				highestBid = bid;
+			}
+			else if (bid > highestBid)
+			{
+				actPrice = highestBid + 1;
+				highestBid = bid;
+				highestBidder = bidder;
+				result += String.format("%s,%d,", highestBidder, actPrice);
+				
+			} else if (bid == highestBid)
+			{
+				actPrice = highestBid;
+				result += String.format("%s,%d,", highestBidder, actPrice);
+			} else
+			{
+				actPrice = bid + 1;
+				result += String.format("%s,%d,", highestBidder, actPrice);
+			}
+		}
+		if (result.length() > 1)
+		{
+			result = Util.removeLastChar(result);
+		}
+		System.out.println(result);
+	}
+	
 	static void level1All() throws FileNotFoundException
 	{
 		ArrayList<String> strs = Util.readlines("data/auction_bidding/in1.txt");
 		for (String str : strs)
 		{
-			level1(str);
+			level1(str.trim());
+		}
+	}
+	
+	static void level2All() throws FileNotFoundException
+	{
+		ArrayList<String> strs = Util.readlines("data/auction_bidding/in2.txt");
+		for (String str : strs)
+		{
+			level2(str.trim());
+		}
+	}
+	
+	static void level3All() throws FileNotFoundException
+	{
+		ArrayList<String> strs = Util.readlines("data/auction_bidding/in3.txt");
+		for (String str : strs)
+		{
+			level3(str.trim());
 		}
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		level1All();
-
+		//level1All();
+		//level2All();
+		level3All();
 	}
 
 }
